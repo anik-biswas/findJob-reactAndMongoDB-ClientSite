c2411e27c616939e3d6a96a0f5fef833c69083c0
anik-biswas <anik.biswas.an@gmail.com>
2023-11-07 23:11:43 -0800

    Home page Extra two section added

--- src/component/employeeSection/EmployeeSection.jsx
+++ src/component/employeeSection/EmployeeSection.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const EmployeeSection = () => {
     return (
-        <div className='my-10'>
-            <h2 className='text-4xl text-center text-gray-600 font-bold'>discover 1000+ inclusive employers</h2>
+        <div>
+            <div className='my-10'>
+            <h2 className='text-xl md:text-2xl lg:text-4xl text-center text-gray-600 font-bold'>discover 1000+ inclusive employers</h2>
             <div className="grid p-10 grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 justify-items-center">
         <div className="card w-64 md:w-72  lg:w-72 bg-amber-100 shadow-xl">
          <div className="card-body">
@@ -52,6 +53,21 @@ const EmployeeSection = () => {
        </div>
        
         </div>
+        </div>
+        <div className='my-10'>
+            <h2 className='text-xl md:text-2xl lg:text-4xl text-center text-gray-600 font-bold'>
+           trusted by pioneering companies</h2>
+           <div className="grid p-10 grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-6 justify-items-center">
+            <img className='rounded-full w-40 h-40' src="https://i.ibb.co/R4XtBYw/denside-logo-design-d-letter-logo-concept-by-abdul-gaffar-dribbble.png" alt="" />
+            <img className='rounded-full w-40 h-40' src="https://i.ibb.co/PTMVP3f/attachment-88710509.png" alt="" />
+            <img className='rounded-full w-40 h-40' src="https://i.ibb.co/MG59bzH/download-1.png" alt="" />
+            <img className='rounded-full w-40 h-40'  src="https://i.ibb.co/Y8ghfxD/n-Vidia-Company-Logo.jpg" alt="" />
+            <img className='rounded-full w-40 h-40' src="https://i.ibb.co/gRnwY0q/download-4.jpg" alt="" />
+            <img className='rounded-full w-40 h-40' src="https://i.ibb.co/QvN3xDT/company-software-logo-vector-30110662.jpg" alt="" />
+           </div>
+
+        </div>
+
         </div>
     );
 };
