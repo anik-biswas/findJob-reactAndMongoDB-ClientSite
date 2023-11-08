785b4fb6bc9a5d11d98c73737b421f52d660734f
anik-biswas <anik.biswas.an@gmail.com>
2023-11-07 18:10:48 -0800

    Delete button worked for My Job page to delete job

--- src/component/myJob/MyJob.jsx
+++ src/component/myJob/MyJob.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../firebase/AuthProvider';
+import Swal from 'sweetalert2';
 
 const MyJob = () => {
     const location = useLocation();
@@ -24,6 +25,42 @@ const MyJob = () => {
         setFilteredJobs(filtered);
     };
     console.log(filterJobs)
+
+    const handleDelete = _id =>{
+        console.log(_id)
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "Delete Job won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/job/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your Job has been deleted.',
+                                'success'
+                            )
+                            // const remainingCart = filterCarts.filter(cart => cart._id !== id);
+                            // setFilteredCart(remainingCart);
+                    
+                        }
+                    })
+
+            }
+        })
+    }
+
     return (
         <div>
             <div className='overflow-x-auto justify-center items-center text-center px-5 md:px-10 lg:px-20'>
@@ -59,7 +96,7 @@ const MyJob = () => {
                                      </button>
                                  </td>
                                  <td>
-                                 <button className="btn">
+                                 <button onClick={() => handleDelete(job._id)} className="btn">
                                          Delete
                                      </button>
                                  </td>
