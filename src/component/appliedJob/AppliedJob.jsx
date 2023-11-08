432e3bc854deea69b79a8dcd92dee9481be5ad6e
anik-biswas <anik.biswas.an@gmail.com>
2023-11-08 08:12:49 -0800

    Apply page delete auto reload added

--- src/component/appliedJob/AppliedJob.jsx
+++ src/component/appliedJob/AppliedJob.jsx
@@ -1,41 +1,81 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../firebase/AuthProvider';
+import Swal from 'sweetalert2';
 
 const AppliedJob = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const { user } = useContext(AuthContext);
     const email = user?.email;
-    const [categories,setCategories] = useState([]);
+    const [categories, setCategories] = useState([]);
     const [applies, setApplies] = useState([]);
-    const [filterApplies, setFilteredApplies] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState('All'); // Initialize with 'All' as the default category
-    useEffect ( () => {
-        fetch('http://localhost:5000/category')
-        .then (res => res.json())
-        .then(data =>setCategories(data))
-        
-    },[])
+
     useEffect(() => {
+        fetch('http://localhost:5000/category')
+            .then(res => res.json())
+            .then(data => setCategories(data));
+    }, []);
+
+    const filterApplied = (applies, email, category) => {
+        return applies.filter((apply) => {
+            return apply.userName === email && (category === 'All' || apply.category === category);
+        });
+    };
+
+    const [filterDelete, setFilterAfterDelete] = useState([]);
+
+    const fetchAppliedJobs = () => {
         fetch('http://localhost:5000/apply')
             .then((res) => res.json())
             .then((data) => {
                 setApplies(data);
-                filterApplied(data, email, selectedCategory);
+                const filteredApplies = filterApplied(data, email, selectedCategory);
+                setFilterAfterDelete(filteredApplies);
             });
-    }, [email, selectedCategory]); 
-
-    const filterApplied = (applies, email, category) => {
-        const filtered = applies.filter((apply) => {
-            return apply.userName === email && (category === 'All' || apply.category === category);
-        });
-        setFilteredApplies(filtered);
     };
+
+    useEffect(() => {
+        fetchAppliedJobs();
+    }, [email, selectedCategory]);
 
     const handleCategoryChange = (e) => {
         const category = e.target.value;
         setSelectedCategory(category);
+    };
+
+    const handleDelete = (_id) => {
+        console.log(_id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "Delete Job won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/apply/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your Job has been deleted.',
+                                'success'
+                            );
+
+                            // After successful deletion, fetch the updated data
+                            fetchAppliedJobs();
+                        }
+                    });
+            }
+        });
     };
 
     return (
@@ -51,59 +91,52 @@ const AppliedJob = () => {
                             onChange={handleCategoryChange}
                         >
                             <option value="All">All</option>
-                            {/* Add options for all available categories */}
-                            {/* <option value="Category1">Category 1</option>
-                            <option value="Category2">Category 2</option> */}
-                            {/* Add more options for each category */}
+
                             {categories.map((category, index) => (
                                 <option key={index} value={category.name}>
-                                {category.name}
+                                    {category.name}
                                 </option>
                             ))}
 
                         </select>
                     </div>
-                  <div className='overflow-x-auto justify-center items-center text-center px-5 md:px-10 lg:px-20'>
-                  <table className="table">
-                        {/* Table header */}
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th className="text-red-400">Title</th>
-                                <th className="text-red-400">Image</th>
-                                <th className="text-red-400">Category</th>
-                                <th className="text-red-400">Salary</th>
-                                <th className="text-red-400">Resume</th>
-                                <th className="text-red-400">Action</th>
-                            </tr>
-                        </thead>
-                        {/* Table body */}
-                        <tbody>
-                            {filterApplies.map((apply, index) => (
-                                <tr key={apply._id}>
-                                    <td>{index + 1}</td>
-                                    <td className="text-xs">{apply.name}</td>
-                                    <td>
-                                        <img src={apply.jobBanner} className="w-10 md:w-16 h-10 md:h-16" alt="" />
-                                    </td>
-                                    <td>{apply.category}</td>
-                                    <td>{apply.salary}</td>
-                                    <td>{apply.cvLink}</td>
-                                    <td>
-                                        <button
-                                            className="btn"
-                                            // Implement the action to remove the applied job here
-                                            // You can use apply._id to identify and remove the job
-                                        >
-                                            X
-                                        </button>
-                                    </td>
+                    <div className='overflow-x-auto justify-center items-center text-center px-5 md:px-10 lg:px-20'>
+                        <table className="table">
+                            {/* Table header */}
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th className="text-red-400">Title</th>
+                                    <th className="text-red-400">Image</th>
+                                    <th className="text-red-400">Category</th>
+                                    <th className="text-red-400">Salary</th>
+                                    <th className="text-red-400">Resume</th>
+                                    <th className="text-red-400">Action</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                  </div>
-                    
+                            </thead>
+                            {/* Table body */}
+                            <tbody>
+                                {filterDelete.map((apply, index) => (
+                                    <tr key={apply._id}>
+                                        <td>{index + 1}</td>
+                                        <td className="text-xs">{apply.name}</td>
+                                        <td>
+                                            <img src={apply.jobBanner} className="w-10 md:w-16 h-10 md:h-16" alt="" />
+                                        </td>
+                                        <td>{apply.category}</td>
+                                        <td>{apply.salary}</td>
+                                        <td>{apply.cvLink}</td>
+                                        <td>
+                                            <button onClick={() => handleDelete(apply._id)} className="btn">
+                                                X
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+
                 </div>
             </div>
         </div>
